69dfa05f4e5aad0ddf07d17601ed9888ba665b25
gerryS2xda <gerardo96bn@hotmail.it>
2019-02-08 20:50:55 +0100

    Strutturazione activity per studente, autista, impiegato e welcome activity. Aggiunta di menu file per ogni tipologia di utente con rispettivo codice Java per usare i menu

---
+++ app/src/main/java/exam/ium/di/easybus/AutistaActivity.java
@@ -0,0 +1,37 @@
+package exam.ium.di.easybus;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class AutistaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autista);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.autista_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.dati_pers_item: break; //I miei dati
+            case R.id.bachAvvItemStud: break; //Vai alla bacheca avvisi
+            case R.id.logout_stud: break; //Logout
+            case R.id.exit_btn: break; //Esci dall'applicazione
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+}
