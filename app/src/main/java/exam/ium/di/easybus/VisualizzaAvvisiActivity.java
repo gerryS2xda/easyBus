38afa4313c902fa6534215c23e81ddbfa57b1dcb
giuseppepaolisi <45846780+giuseppepaolisi@users.noreply.github.com>
2019-02-09 15:45:11 +0100

    crea visualizza avvisi

---
+++ app/src/main/java/exam/ium/di/easybus/VisualizzaAvvisiActivity.java
@@ -0,0 +1,26 @@
+package exam.ium.di.easybus;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ListView;
+
+public class VisualizzaAvvisiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualizza_avvisi);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //lista view
+        ListView lista= findViewById(R.id.listaAvvisi);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
