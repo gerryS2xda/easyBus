b9a9f45fec34acb61397b34b8e3f31a38d7964d3
AntonioCerasuolo <AntonioCerasuolo@users.noreply.github.com>
2019-02-12 22:59:10 +0100

    aggiunto populate biglietto

--- app/src/main/java/exam/ium/di/easybus/BigliettoActivity.java
+++ app/src/main/java/exam/ium/di/easybus/BigliettoActivity.java
@@ -1,38 +1,66 @@
 package exam.ium.di.easybus;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import exam.ium.di.easybus.AdapterForList.AdapterAvviso;
 import exam.ium.di.easybus.AdapterForList.AdapterBiglietto;
-import exam.ium.di.easybus.R;
 import exam.ium.di.easybus.bean.Biglietto;
+import exam.ium.di.easybus.bean.Studente;
 import exam.ium.di.easybus.populate.PopulateBiglietto;
 
-
 public class BigliettoActivity extends AppCompatActivity {
-    private ListView lista;
-    PopulateBiglietto p=new PopulateBiglietto();
-    Biglietto biglietto=new Biglietto();
-    List<Biglietto> biglietti=new ArrayList<Biglietto>();
 
+    private ListView lista;
+    private PopulateBiglietto p=new PopulateBiglietto();
+    private Biglietto biglietto=new Biglietto();
+    private List<Biglietto> biglietti=new ArrayList<Biglietto>();
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_biglietti);
+        setContentView(R.layout.activity_biglietto);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         lista=findViewById(R.id.lista_biglietti);
-        biglietti=p.getLista();
+        biglietti=p.popolaBiglietti(new Studente());
         AdapterBiglietto adapterBiglietto = new AdapterBiglietto(biglietti, this);
 
         lista.setAdapter(adapterBiglietto);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //dialog qrcode
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.layout_dialog);
+        dialog.setTitle("poggia il dispositivo sullo scanner...");
+        ImageView image = (ImageView) dialog.findViewById(R.id.image);
+        Button button=findViewById(R.id.dialogButtonOK);
+        dialog.show();
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
     }
+
+    public void convalida(View view)
+    {
+        Toast.makeText(this,"funonzia",Toast.LENGTH_SHORT).show();
+    }
+
 }
